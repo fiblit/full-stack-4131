39685f84a8d3c111f448687d14bbdddc9008429c
Dalton <hildr039@umn.edu>
2016-09-29 20:48:37 -0500

    Hw4 work

--- hw4/hildr039/adverts.js
+++ hw4/hildr039/adverts.js
@@ -2,17 +2,17 @@
 var adverts = [
   { "file": "Nerve.jpg",
     "URL": "http://sua.umn.edu/events/calendar/event/14781/",
-    "showTime": "Fri, Sep 30 7:00 PM",
+    "showTime": "Fri, Sep 30, 7:00 PM",
     "duration": 7000
   },
   { "file": "secret-life-of-pets.jpg",
     "URL": "http://sua.umn.edu/events/calendar/event/14786/",
-    "showTime": "Fri, Oct 7 7:00 PM",
+    "showTime": "Fri, Oct 7, 7:00 PM",
     "duration": 5000
   },
   { "file": "suicide-squad.jpg",
     "URL": "http://sua.umn.edu/events/calendar/event/14794/",
-    "showTime": "Fri, Oct 14 8:00 PM",
+    "showTime": "Fri, Oct 14, 8:00 PM",
     "duration": 3000
   }
 ];
@@ -22,18 +22,20 @@ var $ = function (id) {
   return document.getElementById(id);
 }
 
-//this function will set the advert
 var interval;
 var currentAd;
+//this function will set the advert
 function setAdvert( i ) {
   clearInterval(interval);
   i %= adverts.length; //This is to prevent overflow, and to auto wrap everywhere
   var img = document.getElementById("advert-image");
   var lnk = document.getElementById("advert-link");
   img.src = "Images\\" + adverts[i].file;
-  img.alt = adverts[i].file.split(".")[0] + "\n" + adverts[i].showTime;
+  var text = adverts[i].file.split(".")[0] + "\n" + adverts[i].showTime;
+  img.title = text;
+  img.alt = text;
   lnk.href = adverts[i].URL;
-  interval = setInterval(setAdvert(i+1), adverts[i].duration);
+  interval = setInterval(function() { setAdvert(i+1); }, adverts[i].duration);
   currentAd = i;
 }
 
