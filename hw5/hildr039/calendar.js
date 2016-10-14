9770d75dcb4baf46df890aff8856b375a7b575ec
Dalton <hildr039@umn.edu>
2016-10-14 17:53:56 -0500

    Hover images

--- hw5/hildr039/calendar.js
+++ hw5/hildr039/calendar.js
@@ -1,32 +1,64 @@
-var todaysEvents = [];
-var eventText = "";
-var day = new Date().getDay();
-var rows = document.getElementsByTagName("tr");
-if (day > 0 && day < 6) {
-  var i;
-  for (i = 0; i < rows[day - 1].cells.length; i++){
-    if (rows[day - 1].cells[i].className === "event-data") {
-      todaysEvents.push(rows[day - 1].cells[i]);
+/* add the daily event extraction */
+function extractTodaysEvents(){
+  var todaysEvents = [];
+  var eventText = "";
+  var day = new Date().getDay();
+  var rows = document.getElementsByTagName("tr");
+  if (day > 0 && day < 6) {
+    var i;
+    for (i = 0; i < rows[day - 1].cells.length; i++){
+      if (rows[day - 1].cells[i].className === "event-data") {
+        todaysEvents.push(rows[day - 1].cells[i]);
+      }
     }
   }
-}
-else {
-  todaysEvents = []; /* nothing on Saturday & Sunday */
+  else {
+    todaysEvents = []; /* nothing on Saturday & Sunday */
+  }
+
+  if (todaysEvents.length > 0) {
+    var i;
+    for (i = 0; i < todaysEvents.length; i++) {
+      var j;
+      for (j = 0; j < todaysEvents[i].children.length; j++) {
+        eventText = eventText.concat(todaysEvents[i].children[j].innerText.toString().concat(". "));
+      }
+      if (i < todaysEvents.length - 1) {
+        eventText = eventText.concat("-/- ");
+      }
+    }
+  }
+  else {
+    eventText = "NO EVENTS TODAY! STUDY! STUDY! STUDY!";
+  }
+  document.getElementsByClassName("days-events")[0].innerHTML = eventText;
 }
 
-if (todaysEvents.length > 0) {
-  var i;
-  for (i = 0; i < todaysEvents.length; i++) {
-    var j;
-    for (j = 0; j < todaysEvents[i].children.length; j++) {
-      eventText = eventText.concat(todaysEvents[i].children[j].innerText.toString().concat(". "));
-    }
-    if (i < todaysEvents.length - 1) {
-      eventText = eventText.concat("-/- ");
-    }
+/* Add the item hover functionality */
+function addImageHover() {
+  entries = document.getElementsByClassName("event-data")
+  for (i = 0; i < entries.length; i++) {
+    entries[i].addEventListener("mouseover", function(self) {
+      return function() {
+        imgs = self.getElementsByClassName("table-preview");
+        if (imgs.length > 0) {
+          imgs[0].className = "table-preview-hover";
+        }
+      };
+    }(entries[i]));
+
+    entries[i].addEventListener("mouseout", function(self) {
+      return function() {
+        imgs = self.getElementsByClassName("table-preview-hover");
+        if (imgs.length > 0) {
+          imgs[0].className = "table-preview";
+        }
+      };
+    }(entries[i]));
   }
 }
-else {
-  eventText = "NO EVENTS TODAY! STUDY! STUDY! STUDY!";
+
+window.onload = function () { 
+  extractTodaysEvents();
+  addImageHover();
 }
-document.getElementsByClassName("days-events")[0].innerHTML = eventText;
