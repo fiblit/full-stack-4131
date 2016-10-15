f5e57886ae96c895eaa919115e7e5c6a469ff274
Dalton Hildreth <hildr039@umn.edu>
2016-10-15 03:10:24 -0500

    Add Dynamic Markers and Infoboxes
    TODO: Bouncing markers and restraunt search

--- hw5/hildr039/map.js
+++ hw5/hildr039/map.js
@@ -2,17 +2,77 @@
 https://developers.google.com/maps/documentation/javascript/examples/geocoding-simple 
 */
 function initMap() {
-  console.log("hello???")
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: {lat: 44.974, lng: -93.234}
+    center: {lat: 44.973, lng: -93.234}
   });
-  console.log(map)
   var geocoder = new google.maps.Geocoder();
 
   document.getElementById('geocode').addEventListener('click', function() {
     geocodeAddress(geocoder, map);
   });
+
+  initMarkers(map);  
+}
+
+function initMarkers(map) {
+  data = getBuildingData();
+  for (var i = 0; i < data.length; i++) {
+    var infowindow = new google.maps.InfoWindow({
+      content: data[i].info
+    });
+
+    var marker = new google.maps.Marker({
+      position: data[i].coord,
+      map: map
+    });
+    infowindow.open(map, marker);
+    marker.addListener('click', function(map, marker, infowindow) { 
+      return function() {
+        infowindow.open(map, marker);
+      }
+    }(map, marker, infowindow));
+  }
+}
+
+function getBuildingData() {
+  var entries = document.getElementsByClassName("event-data")
+  var data = [];
+  for (var i = 0; i < entries.length; i++) {
+    var event = entries[i].getElementsByClassName("event")[0].innerText;
+    var build = entries[i].getElementsByClassName("loc")[0].innerText;
+    var info = "<p><b>" + event + "</b></p>" +
+    "<p>" + build + "</p>";
+
+    var coordLatLng = entries[i].getElementsByClassName("coord")[0].innerText.split(" ");
+    var coord = {lat: Number(coordLatLng[0]), lng: Number(coordLatLng[1])};
+
+    var hasCoord = false;
+    for (var c = 0; c < data.length; c++) {
+      if (data[c].coord.lat === coord.lat && data[c].coord.lng === coord.lng) { //already has location
+        var hasEvent = false;
+        for (var j = 0; j < data[c].info.length; j++) {
+          if (data[c].info[j] === info) { //already has event
+            hasEvent = true;
+            break;
+          }
+        }
+        if (!hasEvent) {
+          data[c].info.push(info);
+        }
+        hasCoord = true;
+        break;
+      }
+    }
+    if (!hasCoord) {
+      data.push({coord: coord, info: [info]});
+    }
+  }
+  for (var d = 0; d < data.length; d++) {
+    data[d].info = data[d].info.join().replace(/,/g,"");
+  }
+
+  return data;
 }
 
 function geocodeAddress(geocoder, resultsMap) {
@@ -29,4 +89,3 @@ function geocodeAddress(geocoder, resultsMap) {
     }
   });
 }
-/* end of modified code code */
